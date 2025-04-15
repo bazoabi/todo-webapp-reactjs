f5ef23e854bd5ba84bc718ff87b68c2af10efba9
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-15 10:59:02 +0200

    Refactor: Moving all the dialogs modals up to TodoList

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,6 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
@@ -13,21 +11,8 @@ import CheckIcon from "@mui/icons-material/Check";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 
-// Dialogs
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import TextField from "@mui/material/TextField";
-
 // Styles
 import "../App.css";
-
-// Hooks + Others
-import { useState } from "react";
-import { TodosContext } from "../contexts/TodosContext";
-import { useContext } from "react";
 
 const nonCheckedbtnStyle = {
   color: "#8bc34a",
@@ -41,168 +26,13 @@ const checkedBtnStyle = {
   border: "solid #c9d0c1 3px",
 };
 
-export default function Todo({ todo }) {
-  const { todos, setTodos } = useContext(TodosContext);
-
-  // Delete Dialog Logic //
-  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
-
-  const handleClickOpenDeleteDialog = () => {
-    setOpenDeleteDialog(true);
-  };
-
-  const handleCloseDeleteDialog = (action) => {
-    if (action === "Agree") {
-      // Delete the todo
-      const updatedTodos = todos.filter((t) => {
-        return t.id !== todo.id;
-      });
-      setTodos(updatedTodos);
-      // Update the local storage with the new todos array
-      localStorage.setItem("todos", JSON.stringify(updatedTodos));
-    }
-    setOpenDeleteDialog(false);
-  };
-
-  function DeleteDialog() {
-    return (
-      <Dialog
-        sx={{ direction: "rtl" }}
-        open={openDeleteDialog}
-        onClose={handleCloseDeleteDialog}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"מחיקת משימה"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            האם תרצה למחוק את המשימה?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => handleCloseDeleteDialog("Disagree")}
-            sx={{ color: "green" }}
-          >
-            לא
-          </Button>
-          <Button
-            onClick={() => handleCloseDeleteDialog("Agree")}
-            autoFocus
-            sx={{ color: "red" }}
-          >
-            כן
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
-  // === Delete Dialog Logic === //
-
-  // Update Dialog Logic //
-  const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
-
-  const handleClickOpenUpdateDialog = () => {
-    setOpenUpdateDialog(true);
-  };
-
-  function UpdateDialog() {
-    const [updatedTodo, setUpdatedTodo] = useState({
-      title: todo.title,
-      details: todo.details,
-    });
-
-    const handleCloseUpdateDialog = (action) => {
-      if (action === "Agree") {
-        // Update todo
-        const updatedTodos = todos.map((t) => {
-          if (t.id === todo.id) {
-            return {
-              ...t,
-              title: updatedTodo.title,
-              details: updatedTodo.details,
-            };
-          } else {
-            return t;
-          }
-        });
-        setTodos(updatedTodos);
-        localStorage.setItem("todos", JSON.stringify(updatedTodos));
-      }
-      setOpenUpdateDialog(false);
-    };
-
-    return (
-      <Dialog
-        sx={{ direction: "rtl" }}
-        open={openUpdateDialog}
-        onClose={handleCloseUpdateDialog}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"עריכת משימה"}</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="todoTitle"
-            name="todoTitle"
-            label="שם המשימה"
-            type="text"
-            fullWidth
-            variant="standard"
-            value={updatedTodo.title}
-            onChange={(e) =>
-              setUpdatedTodo({ ...updatedTodo, title: e.target.value })
-            }
-          />
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="todoDetails"
-            name="todoDetails"
-            label="תיאור המשימה"
-            multiline
-            type="text"
-            fullWidth
-            variant="standard"
-            value={updatedTodo.details}
-            onChange={(e) =>
-              setUpdatedTodo({ ...updatedTodo, details: e.target.value })
-            }
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => handleCloseUpdateDialog("Disagree")}
-            sx={{ color: "red" }}
-          >
-            ביטול
-          </Button>
-          <Button
-            onClick={() => handleCloseUpdateDialog("Agree")}
-            autoFocus
-            sx={{ color: "green" }}
-          >
-            עדכון
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
-  // === Update Dialog Logic === //
-
-  // Toggle Completed Logic
-  function toggleCompleted(id) {
-    const updatedTodos = todos.map((t) =>
-      t.id === id ? { ...t, completed: !t.completed } : t
-    );
-    setTodos(updatedTodos);
-    // Update the local storage with the new todos array
-    localStorage.setItem("todos", JSON.stringify(updatedTodos));
-  }
+export default function Todo({
+  todo,
+  handleClickOpenDeleteDialog,
+  handleClickOpenUpdateDialog,
+  handleToggleCompleteTodo,
+}) {
+  // const { todos, setTodos } = useContext(TodosContext);
 
   return (
     <>
@@ -247,7 +77,7 @@ export default function Todo({ todo }) {
                 className="iconButton"
                 aria-label="Mark task as completed"
                 sx={todo.completed ? checkedBtnStyle : nonCheckedbtnStyle}
-                onClick={() => toggleCompleted(todo.id)}
+                onClick={() => handleToggleCompleteTodo(todo.id)}
               >
                 <CheckIcon />
               </IconButton>
@@ -262,7 +92,7 @@ export default function Todo({ todo }) {
                   color: "#1769aa",
                   border: "solid #1769aa 3px",
                 }}
-                onClick={handleClickOpenUpdateDialog}
+                onClick={() => handleClickOpenUpdateDialog(todo)}
               >
                 <EditOutlinedIcon />
               </IconButton>
@@ -277,13 +107,12 @@ export default function Todo({ todo }) {
                   color: "#b23c17",
                   border: "solid #b23c17 3px",
                 }}
-                onClick={handleClickOpenDeleteDialog}
+                onClick={() => handleClickOpenDeleteDialog(todo)}
               >
                 <DeleteOutlineOutlinedIcon />
               </IconButton>
               {/* ==== Delete Icon Button ==== */}
-              <DeleteDialog />
-              <UpdateDialog />
+              {/* <UpdateDialog /> */}
               {/* Delete Confirmation Dialog */}
 
               {/* ==== Delete Confirmation Dialog ==== */}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,14 +16,22 @@ import { useEffect, useMemo } from "react";
 // Components
 import Todo from "./Todo";
 
+// Dialogs
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 // Others
 import { v4 as uuidv4 } from "uuid";
 import { TodosContext } from "../contexts/TodosContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function TodoList() {
   // console.log("TodoList Rendered");
   const { todos, setTodos } = useContext(TodosContext);
+  const [activeDialogTodo, setActiveDialogTodo] = useState(null);
 
   // Fetch Todos from localStorage after mounting the TodoList for the first time
   useEffect(() => {
@@ -32,6 +40,171 @@ export default function TodoList() {
       setTodos(JSON.parse(storedTodos));
     }
   }, []);
+
+  // Delete Dialog Logic //
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+  const handleClickOpenDeleteDialog = (todo) => {
+    setOpenDeleteDialog(true);
+    setActiveDialogTodo(todo);
+  };
+
+  const handleCloseDeleteDialog = (action) => {
+    if (action === "Agree") {
+      // Delete the todo
+      const updatedTodos = todos.filter((t) => {
+        return t.id !== activeDialogTodo.id;
+      });
+      setTodos(updatedTodos);
+      // Update the local storage with the new todos array
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
+    setOpenDeleteDialog(false);
+    setActiveDialogTodo(null);
+  };
+
+  function DeleteDialog() {
+    return (
+      <Dialog
+        sx={{ direction: "rtl" }}
+        open={openDeleteDialog}
+        onClose={handleCloseDeleteDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"מחיקת משימה"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            האם תרצה למחוק את המשימה?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => handleCloseDeleteDialog("Disagree")}
+            sx={{ color: "green" }}
+          >
+            לא
+          </Button>
+          <Button
+            onClick={() => handleCloseDeleteDialog("Agree")}
+            autoFocus
+            sx={{ color: "red" }}
+          >
+            כן
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+  // === Delete Dialog Logic === //
+
+  // Update Dialog Logic //
+  const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
+
+  const handleClickOpenUpdateDialog = (todo) => {
+    setOpenUpdateDialog(true);
+    setActiveDialogTodo(todo);
+  };
+
+  function UpdateDialog() {
+    const [updatedTodo, setUpdatedTodo] = useState({
+      title: activeDialogTodo?.title,
+      details: activeDialogTodo?.details,
+    });
+
+    const handleCloseUpdateDialog = (action) => {
+      if (action === "Agree") {
+        // Update todo
+        const updatedTodos = todos.map((t) => {
+          if (t.id === activeDialogTodo.id) {
+            return {
+              ...t,
+              title: updatedTodo.title,
+              details: updatedTodo.details,
+            };
+          } else {
+            return t;
+          }
+        });
+        setTodos(updatedTodos);
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+      }
+      setOpenUpdateDialog(false);
+      setActiveDialogTodo(null);
+    };
+
+    return (
+      <Dialog
+        sx={{ direction: "rtl" }}
+        open={openUpdateDialog}
+        onClose={handleCloseUpdateDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"עריכת משימה"}</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="todoTitle"
+            name="todoTitle"
+            label="שם המשימה"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={updatedTodo.title}
+            onChange={(e) =>
+              setUpdatedTodo({ ...updatedTodo, title: e.target.value })
+            }
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="todoDetails"
+            name="todoDetails"
+            label="תיאור המשימה"
+            multiline
+            type="text"
+            fullWidth
+            variant="standard"
+            value={updatedTodo.details}
+            onChange={(e) =>
+              setUpdatedTodo({ ...updatedTodo, details: e.target.value })
+            }
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => handleCloseUpdateDialog("Disagree")}
+            sx={{ color: "red" }}
+          >
+            ביטול
+          </Button>
+          <Button
+            onClick={() => handleCloseUpdateDialog("Agree")}
+            autoFocus
+            sx={{ color: "green" }}
+          >
+            עדכון
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+  // === Update Dialog Logic === //
+
+  // Toggle Complete Task Logic //
+  function handleToggleCompleteTodo(id) {
+    const updatedTodos = todos.map((t) =>
+      t.id === id ? { ...t, completed: !t.completed } : t
+    );
+    setTodos(updatedTodos);
+    // Update the local storage with the new todos array
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  }
+  // ==== Toggle Complete Task Logic ==== //
 
   function handleAddClick() {
     console.log("Add Clicked");
@@ -74,7 +247,15 @@ export default function TodoList() {
     todosToBeRendered = notCompletedTodosList;
   }
   const TodosJsx = todosToBeRendered.map((todo) => {
-    return <Todo key={todo.id} todo={todo} />;
+    return (
+      <Todo
+        key={todo.id}
+        todo={todo}
+        handleClickOpenDeleteDialog={handleClickOpenDeleteDialog}
+        handleClickOpenUpdateDialog={handleClickOpenUpdateDialog}
+        handleToggleCompleteTodo={handleToggleCompleteTodo}
+      />
+    );
   });
   // Todo Items Logic //
 
@@ -153,6 +334,11 @@ export default function TodoList() {
             </Grid>
           </CardContent>
         </Card>
+
+        {/* Dialogs */}
+        <DeleteDialog />
+        <UpdateDialog />
+        {/* === Dialogs === */}
       </Container>
     </>
   );
