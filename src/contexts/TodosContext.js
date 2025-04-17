8b3ee88d9a9d2f61efdf97620ee27eb06b553394
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-17 20:26:27 +0200

    Convert the TodosContext to Provider

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -1,6 +1,43 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import TodosReducer from "../reducers/TodosReducer";
+import { v4 as uuidv4 } from "uuid";
 
-export const TodosContext = createContext({
+const initialTodos = [
+  {
+    id: uuidv4(),
+    title: "משימה 1",
+    details: "תיאור המשימה 1",
+    completed: false,
+  },
+  {
+    id: uuidv4(),
+    title: "משימה 2",
+    details: "תיאור המשימה 2",
+    completed: true,
+  },
+  {
+    id: uuidv4(),
+    title: "משימה 3",
+    details: "תיאור המשימה 3",
+    completed: false,
+  },
+];
+
+const TodosContext = createContext({
   todos: [],
   setTodos: () => {},
 });
+
+export const TodosProvider = ({ children }) => {
+  const [todos, todosDispatch] = useReducer(TodosReducer, initialTodos);
+
+  return (
+    <TodosContext.Provider value={{ todos, todosDispatch }}>
+      {children}
+    </TodosContext.Provider>
+  );
+};
+
+export const useTodos = () => {
+  return useContext(TodosContext);
+};
